91f4dd25a3f1341c46ad14ab6ed38bc5d6c929f6
nasa1515 <h43254@gmail.com>
2021-07-20 18:15:11 +0900

    Commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,7 +48,6 @@ export const pageQuery = graphql`
             links {
               post
               github
-              demo
               googlePlay
               appStore
             }
