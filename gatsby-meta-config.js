c97fe65ff0b7fcd6e1a0591f82d42165d260cdc3
nasa1515 <h43254@gmail.com>
2021-07-20 17:43:16 +0900

    Commit

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,11 +2,11 @@ module.exports = {
   title: `나사블로그`,
   description: `나사블로그`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://nasa1515.tech`,
   ogImage: `/PINGGU.jpg`, // Path to your image you placed in the 'static' folder
   comments: {
     utterances: {
-      repo: `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `nasa1515/nasablog`,
     },
   },
   ga: 'UA-134826755-2', // Google Analytics Tracking ID
@@ -14,13 +14,12 @@ module.exports = {
     name: `NASA1515`,
     bio: {
       role: `Data Engineer`,
-      description: ['사람을 좋아하는', '편리함을 만드는'],
+      description: ['데이터를 좋아하는', '엔지니어 특화'],
       isVideo: true,
     },
     social: {
-      github: `https://github.com/zoomKoding`,
-      linkedIn: `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: `zoomkoding@gmail.com`,
+      github: `https://github.com/nasa1515`,
+      linkedIn: `https://www.linkedin.com/in/wonseok-lee-60b9011a3`,
     },
   },
 
@@ -51,8 +50,8 @@ module.exports = {
         },
       },
       {
-        date: '2021.03 ~ ',
-        activity: '우아한 개발자🚀',
+        date: '2020.12 ~ ',
+        activity: 'Cloocus Data Enginner🚀',
       },
     ],
 
@@ -73,30 +72,6 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
-      {
-        title: 'Picky(글로벌 스킨케어 제품 분석 모바일 앱) 개발',
-        description:
-          '구글 출신 분들이 창업한 스타트업에 인턴십을 하던 중 피버팅을 하게 되면서 저는 1인 개발자로 신규 사업에 참여하게 되었습니다. 5명의 경험이 많은 팀원들과 힘을 합쳐 Picky라는 앱을 개발하고 지속적으로 서비스를 확장해나갔습니다. 이 과정을 통해 실제 서비스의 시작과 성장하는 과정을 경험해볼 수 있었습니다.',
-        techStack: ['flutter', 'nodejs'],
-        thumbnailUrl: 'picky.jpeg',
-        links: {
-          post: '/start-up-app-development',
-          googlePlay: 'https://play.google.com/store/apps/details?id=care.jivaka.picky',
-          appStore: 'https://apps.apple.com/app/picky-skincare-made-smarter/id1504197356',
-        },
-      },
-      {
-        title: '개발 블로그 테마 개발',
-        description:
-          '간단한 테마를 활용하여 개발 블로그를 만들고 운영하다 보니 점점 블로그를 내가 원하는 형태로 만들고 싶게 되었습니다. 입사 전 시기를 활용해서 원하는 기능과 디자인이 있는 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'zoomkoding.png',
-        links: {
-          post: '/why-dev-blog',
-          github: 'https://github.com/zoomkoding/college-timetable',
-          demo: 'https://gatsby-starter-zoomkoding.netlify.app',
-        },
-      },
       {
         title: '2020 우아한테크캠프 참여',
         description:
