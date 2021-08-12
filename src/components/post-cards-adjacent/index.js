11b10c787adccf4a99873761268de767e79e61f8
nasa1515 <h43254@gmail.com>
2021-08-12 18:08:25 +0900

    Commit

--- src/components/post-cards-adjacent/index.js
+++ src/components/post-cards-adjacent/index.js
@@ -7,16 +7,16 @@ const PostCardsAdjacent = ({ prevPost, nextPost }) => {
   return (
     <div className="post-cards-adjacent-wrapper">
       <div className="post-cards-adjacent">
-        {prevPost && (
-          <Link className="post-card prev" key={prevPost.id} to={prevPost.slug}>
-            <div className="direction">다음 글</div>
-            <div className="title">{prevPost.title}</div>
-          </Link>
-        )}
         {nextPost && (
           <Link className="post-card next" key={nextPost.id} to={nextPost.slug}>
             <div className="direction">이전 글</div>
             <div className="title">{nextPost.title}</div>
+          </Link>
+        )}
+        {prevPost && (
+          <Link className="post-card prev" key={prevPost.id} to={prevPost.slug}>
+            <div className="direction">다음 글</div>
+            <div className="title">{prevPost.title}</div>
           </Link>
         )}
       </div>
