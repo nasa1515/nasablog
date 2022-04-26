1fcf2565b49056a25ff27973b6821fdec5ae2751
nasa1515 <h43254@gmail.com>
2022-04-26 18:20:46 +0900

    COMMIT

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: `DE NASA1515 BLOG`,
-  description: `NASA1515 BLOG`,
+  title: `Data Engineer NASA1515 BLOG`,
+  description: `Data Engineer NASA1515 BLOG`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
   siteUrl: `https://nasa1515.com`,
   ogImage: `/topicon.jpg`, // Path to your image you placed in the 'static' folder
